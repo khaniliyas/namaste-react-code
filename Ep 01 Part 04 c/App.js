30e6aafedc60cb7b77423df4e5b543442647a581
Iliyas Khan <iliyaskhanh94@gmail.com>
2023-08-27 10:39:58 +0530

    how to create two child div that has two child?

---
+++ Ep 01 Part 04 c/App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1 element"),
+    React.createElement("h2", {}, "h2 element"),
+  ]),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "h1 element"),
+    React.createElement("h2", {}, "h2 element"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
